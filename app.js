fd9b4f16c088d1be9ec16ad8a0cba519777c0fd3
Vivek Sahu <vivek@Viveks-MacBook-Pro.local>
2023-11-04 12:49:44 +0530

    initial code

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-{
-  /* <div id="parent">
-  <div id="child">
-    <h1>I'm a H1 Tag</h1>
-  </div>
-</div>; */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "heading" }, "Hello World from React")
-  )
-);
+const Child = () => <h1>Namaste React child component</h1>;
+
+const Parent = () => {
+  return (
+    <>
+      <Child />
+      <h1>Namaste React parent component</h1>
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Parent />);
